43c66eb5a1aee6297fe0a00f62b050762ef6eb0a
ashirwad5555 <ashirwadkatkamwar@gmail.com>
2025-06-15 19:34:31 +0530

    vercel added

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -3,7 +3,8 @@ class ApiConfig {
   // static const String baseUrl = 'http://10.0.2.2:5000';
 
   // For testing on a physical device, use your computer's IP address
-  static const String baseUrl = 'http://192.168.109.134:5000';
+  // static const String baseUrl = 'http://192.168.109.134:5000';
+  static const String baseUrl = 'https://mini-books-tan.vercel.app';
   // static const String baseUrl = 'https://minibooks.onrender.com';
 
   // For production
